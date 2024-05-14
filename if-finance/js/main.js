3848a28b53638f6764ffa61465d23e70226897f5
Renan Cavichi <renancavichi@gmail.com>
2024-05-13 22:06:31 -0300

    add api form to create stock card

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,5 +1,8 @@
 console.log("Arquivo js externo!") // loga uma mensagem no console
 
+// adicione seu token
+const token = 
+
 const allStocks = [
 	{ 
 		bolsa: "NASDAQ",
@@ -100,15 +103,22 @@ function loadTable(){
 	table.innerHTML = rows
 }
 
-const openModal = () => {
-	const modal = document.getElementById('add-card-modal')
+const openModal = (idModal) => {
+	const modal = document.getElementById(idModal)
 	modal.style.display = 'flex'
 }
 
 const closeModal = (event, id) => {
-	const modal = document.getElementById('add-card-modal')
-	if(event?.target?.id === 'add-card-modal' || id === 'add-card-modal' ){
+	if(id){
+		const modal = document.getElementById(id)
 		modal.style.display = 'none'
+		return
+	}
+
+	if(event?.target?.className === "modal"){
+		const modal = document.getElementById(event.target.id)
+		modal.style.display = 'none'
+		return
 	}
 }
 
@@ -131,5 +141,40 @@ const createCard = (event) =>{
 
 	addCard(stock)
 	event.target.reset()
-	closeModal(null, 'add-card-modal')
+	closeModal(null, 'add-form-modal')
+}
+
+// async function testeApi(){
+// }
+const testeApi = async () => {
+	const response = await fetch('https://finnhub.io/api/v1/quote?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+	//const response = await fetch('https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+	
+	console.log(response)
+	const result = await response.json()
+	console.log(result.objectName)
+}
+
+const createApiCard = async (event) =>{
+	event.preventDefault()
+	const {codigo, nAcoes} = event.target.elements
+
+	const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
+	const result = await response.json()
+
+	const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
+	const profile = await response2.json()
+
+	const stock = {
+		bolsa: profile.exchange.split(' ')[0],
+		codigo: codigo.value,
+		empresa: profile.name,
+		valor: result.c * 100,
+		variacao: result.d,
+		nAcoes: nAcoes.value
+	}
+
+	addCard(stock)
+	event.target.reset()
+	closeModal(null, 'add-api-modal')
 }
